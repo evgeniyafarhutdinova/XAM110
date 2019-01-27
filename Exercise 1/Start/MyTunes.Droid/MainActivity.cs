6a7acc8517321458c1c89222d9d68d1462809f19
farkhutdinova <evgeniyafarhutdinova@gmail.com>
2019-01-27 20:47:16 +0300

    used conditional compilation

--- Exercise 1/Start/MyTunes.Droid/MainActivity.cs
+++ Exercise 1/Start/MyTunes.Droid/MainActivity.cs
@@ -1,20 +1,29 @@
 ﻿using Android.App;
 using Android.OS;
+using System.Linq;
 
 namespace MyTunes
 {
-	[Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : ListActivity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : ListActivity
+    {
+        protected override async void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			ListAdapter = new ListAdapter<string>() {
-				DataSource = new[] { "One", "Two", "Three" }
-			};
-		}
-	}
+            /*ListAdapter = new ListAdapter<string>()
+            {
+                DataSource = new[] { "One", "Two", "Three" }
+            };*/
+            var songs = await SongLoader.Load();
+            var songsList = songs.ToList();
+
+            ListAdapter = new ListAdapter<Song>()
+            {
+                DataSource = songsList,
+                TextProc = s => s.Name,
+                DetailTextProc = s => $"{s.Artist} - {s.Album}"
+            };
+        }
+    }
 }
-
-
